fbd35ae2ee853a7fc0525cf7d4505cf8a9eba3b5
ches0 <ches0@ChesModern14>
2022-09-16 19:11:26 +0900

    220916 training

--- JavaPrograming/src/main.java
+++ JavaPrograming/src/main.java
@@ -1,9 +1,51 @@
-
+import java.util.Scanner;
 public class main {
-
 	public static void main(String[] args) {
-		// hello?
-
+		var name = GetData.get_name();
+		var city = GetData.get_city();
+		var age = GetData.get_age();
+		var weight = GetData.get_weight();
+		var is_marry = GetData.get_is_marry();
+		
+		
+		System.out.print("이름 : "+name);
+		System.out.print(" 도시 : "+city);
+		System.out.printf(" 나이 : %d",age);
+		System.out.print(" 몸무게 : "+weight);
+		System.out.print(" 결혼여부 : ");
+		if(is_marry) {
+			System.out.print("결혼함");
+		}
+		else {
+			System.out.print("미혼");
+		}
 	}
 
+}
+class GetData{
+	public static String get_name() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("이름을 입력 하시오 : ");
+		return scanner.next();
+	}
+	public static String get_city() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("도시를 입력하시오 : ");
+		return scanner.next();
+	}
+	public static int get_age() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("나이를 입력하시오 : ");
+		return scanner.nextInt();
+	}
+	public static double get_weight() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("체중를 입력하시오 : ");
+		return scanner.nextDouble();
+	}
+	public static boolean get_is_marry() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("결혼여부를 입력하시오 : ");
+		return scanner.nextBoolean();
+	}
 }
