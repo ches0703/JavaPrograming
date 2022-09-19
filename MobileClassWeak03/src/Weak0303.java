dc3f259c27808a585415d0124adb79b6fa17a627
ches0703 <ches0703@naver.com>
2022-09-19 11:14:23 +0900

    Weak0303 & MobileClass20220919 Training make

--- MobileClassWeak03/src/Weak0303.java
+++ MobileClassWeak03/src/Weak0303.java
@@ -3,5 +3,22 @@ public class Weak0303 {
 
 	public static void main(String[] args) {
 
+		SumEvenNum();
+		
 	}
+	
+	public static void SumEvenNum() {
+		int sum = 0;
+		for(int i=0;;i++) {
+			if(i>100) {
+				break;
+			}
+			if(i%2==1) {
+				continue;
+			}
+			sum += i;
+		}
+		System.out.println("Sum of Even Num in 1 to 100 : " + sum);
+	}
+	
 }
