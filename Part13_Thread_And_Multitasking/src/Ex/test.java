ea2c99a87f25532f2dea0e0023e100f2824a1b61
ches0703 <ches0703@naver.com>
2022-12-13 02:44:34 +0900

    Hello

---
+++ Part13_Thread_And_Multitasking/src/Ex/test.java
@@ -0,0 +1,21 @@
+package Ex;
+
+public class test {
+    
+    public static void main(String argc[]){
+        System.out.println("Hello ? ");
+
+        int i = Integer.valueOf(10);
+
+
+        for(int j = 0; j < 10; j++){
+            System.out.println(i);
+            i++;
+        }
+
+
+
+        
+    }
+
+}
